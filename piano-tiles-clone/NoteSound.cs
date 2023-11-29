1dd657b24899b4605256ea966c9592a2856284c2
KeatonGithub <keatonkp7777@gmail.com>
2023-11-29 17:25:35 -0500

    Create Attributes in NoteSound

--- piano-tiles-clone/NoteSound.cs
+++ piano-tiles-clone/NoteSound.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Raylib_cs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace piano_tiles_clone
 {
     internal class NoteSound
     {
+        Sound[] sounds = new Sound[4];
+        Sound buzzer;
+        public NoteSound() 
+        {
+
+       }  
     }
 }
