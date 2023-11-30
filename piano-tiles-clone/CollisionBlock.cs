9adda1e99a889a3046f463e3b931503e66886701
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-11-30 15:11:57 -0500

    Add Comments

--- piano-tiles-clone/CollisionBlock.cs
+++ piano-tiles-clone/CollisionBlock.cs
@@ -11,10 +11,12 @@ namespace piano_tiles_clone
 {
     public class CollisionBlock
     {
+        // Collision block attributes  
         Vector2 position;
         Vector2 size;
         Color color;
 
+        // When CollisionBlock is created set size, position, and color from passed in values
         public CollisionBlock(int positionX, int positionY, Color color)
         {
             size.X = 100;
@@ -25,34 +27,38 @@ namespace piano_tiles_clone
 
         }
 
+        // Draw to screen
         public void Draw()
         {
             Raylib.DrawRectangleV(position, size, color);
         }
 
+        // Checks if a MusicNote object has collided with a CollisionBlock object
         public bool CollideWithNote(MusicNote note)
         {
             bool noteCollide = false;
             Vector2 notePosition = note.GetPosition();
 
+            // If the MusicNote Y position is inside the Y position and the Y position + Y size for the CollisionBlock
+            // check to see if the correct key was pressed
             if (notePosition.Y > (position.Y - 2) && notePosition.Y < (position.Y + size.Y + 2))
             {
-                if (note.GetNoteValue == 0 && Raylib.IsKeyPressed(KeyboardKey.KEY_Q))
-                { 
-                    noteCollide = true; 
-                }
-
-                if (note.GetNoteValue == 1 && Raylib.IsKeyPressed(KeyboardKey.KEY_W))
+                if (note.GetNoteValue() == 0 && Raylib.IsKeyPressed(KeyboardKey.KEY_Q))
                 {
                     noteCollide = true;
                 }
 
-                if (note.GetNoteValue == 2 && Raylib.IsKeyPressed(KeyboardKey.KEY_E))
+                if (note.GetNoteValue() == 1 && Raylib.IsKeyPressed(KeyboardKey.KEY_W))
                 {
                     noteCollide = true;
                 }
 
-                if (note.GetNoteValue == 3 && Raylib.IsKeyPressed(KeyboardKey.KEY_R))
+                if (note.GetNoteValue() == 2 && Raylib.IsKeyPressed(KeyboardKey.KEY_E))
+                {
+                    noteCollide = true;
+                }
+
+                if (note.GetNoteValue() == 3 && Raylib.IsKeyPressed(KeyboardKey.KEY_R))
                 {
                     noteCollide = true;
                 }
