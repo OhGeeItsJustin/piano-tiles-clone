a026662c8fb4087cfd98a7de011b46567d29df59
KeatonGithub <keatonkp7777@gmail.com>
2023-11-30 12:44:23 -0500

    Add W,E,R Sounds and Buzzer

--- piano-tiles-clone/NoteSound.cs
+++ piano-tiles-clone/NoteSound.cs
@@ -16,14 +16,38 @@ namespace piano_tiles_clone
         {
             InitAudioDevice();
             sounds[0] = LoadSound("../../../SoundAssets/QSound.wav");
-       }
+            sounds[1] = LoadSound("../../../SoundAssets/WSound.wav");
+            sounds[2] = LoadSound("../../../SoundAssets/ESound.wav");
+            sounds[3] = LoadSound("../../../SoundAssets/RSound.wav");
+            buzzer = LoadSound("../../../SoundAssets/Buzzer.wav");
+        }
         public void PlayQSound() 
         {
             PlaySound(sounds[0]);
         }
+        public void PlayWSound()
+        {
+            PlaySound(sounds[1]);
+        }
+        public void PlayESound()
+        {
+            PlaySound(sounds[2]);
+        }
+        public void PlayRSound()
+        {
+            PlaySound(sounds[3]);
+        }
+        public void PlayBuzzer()
+        {
+            PlaySound(buzzer);
+        }
         public void EndSounds()
         {
             UnloadSound(sounds[0]);
+            UnloadSound(sounds[1]);
+            UnloadSound(sounds[2]);
+            UnloadSound(sounds[3]);
+            UnloadSound(buzzer);
             CloseAudioDevice();
         }
     }
