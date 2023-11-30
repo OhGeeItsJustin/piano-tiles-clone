e7145bbabf326a20899a830a480ea4c076afe2a5
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-11-30 14:42:32 -0500

    Create CollisionBlock Attributes

--- piano-tiles-clone/CollisionBlock.cs
+++ piano-tiles-clone/CollisionBlock.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
 
 namespace piano_tiles_clone
 {
     public class CollisionBlock
     {
+        Vector2 position;
+        Vector2 size;
+        Color color;
+
     }
 }
