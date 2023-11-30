55c2d0e26dc402285ecb834369ca3954b9da274b
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-11-30 15:01:49 -0500

    Create CollideWithNote Function

--- piano-tiles-clone/CollisionBlock.cs
+++ piano-tiles-clone/CollisionBlock.cs
@@ -30,5 +30,36 @@ namespace piano_tiles_clone
             Raylib.DrawRectangleV(position, size, color);
         }
 
+        public bool CollideWithNote(MusicNote note)
+        {
+            bool noteCollide = false;
+            Vector2 notePosition = note.GetPosition();
+
+            if (notePosition.Y > (position.Y - 2) && notePosition.Y < (position.Y + size.Y + 2))
+            {
+                if (note.GetNoteValue == 0 && Raylib.IsKeyPressed(KeyboardKey.KEY_Q))
+                { 
+                    noteCollide = true; 
+                }
+
+                if (note.GetNoteValue == 1 && Raylib.IsKeyPressed(KeyboardKey.KEY_W))
+                {
+                    noteCollide = true;
+                }
+
+                if (note.GetNoteValue == 2 && Raylib.IsKeyPressed(KeyboardKey.KEY_E))
+                {
+                    noteCollide = true;
+                }
+
+                if (note.GetNoteValue == 3 && Raylib.IsKeyPressed(KeyboardKey.KEY_R))
+                {
+                    noteCollide = true;
+                }
+            }
+
+            return noteCollide;
+        }
+
     }
 }
